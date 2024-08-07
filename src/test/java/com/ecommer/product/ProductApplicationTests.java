897a6fc6b0dcb503aee84272d7715f1a07347fc4
tjdanqkr <tjdanqkr@gmail.com>
2024-08-07 18:14:10 +0900

    [init] sonar and jenkins setting

--- src/test/java/com/ecommer/product/ProductApplicationTests.java
+++ src/test/java/com/ecommer/product/ProductApplicationTests.java
@@ -1,13 +1,6 @@
 package com.ecommer.product;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+class ProductApplicationTests {}
